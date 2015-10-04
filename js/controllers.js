e8f3fe32a575f261fa2c43f877e67df2599b23f6
juniar.rakhman <juniar.rahman@gmail.com>
2015-10-05 00:08:45 +0800

    chapter 3

--- js/controllers.js
+++ js/controllers.js
@@ -3,5 +3,6 @@ var myNameSpace = angular.module('myApp', []);
 myNameSpace.controller('MyController', ['$scope', '$http', function myController($scope, $http) {
     $http.get('js/data.json').success(function (data) {
         $scope.artists = data;
+        $scope.artistOrder = 'name';
     });
 }]);
